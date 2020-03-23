6f1545e2e9fa52e1cf3151362b44e761b2deaa62
877chs7oe99hk9wf <admin@linuxmasterrace.com>
2020-03-23 17:56:56 +0200

    add activity graph to dashboard

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -44,8 +44,44 @@
             </div>
             <div class="col card">
             Activity from the past week
-            
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
 
+            <canvas id="activityChart" width="400" height="400"></canvas>
+            <script>
+            var ctx = document.getElementById('activityChart');
+            var activityChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    <?php
+                        echo "labels: ['".(date("d") - 6)."', '".(date("d") - 5)."', '".(date("d") - 4)."', '".(date("d") - 3)."', '".(date("d") - 2)."', '".(date("d") - 1)."', '".date("d")."'],";
+                    ?>
+                    datasets: [{
+                        label: '# of transactions',
+                        <?php
+                                $txvol = [0, 0, 0, 0, 0, 0, 0];
+                            foreach ($history as $row)
+                            {
+                                $row = var_export($row->tx_date, true);
+                                $date = new DateTime();
+                                for ($x = 0; $x <= 5; $x++) {
+                                    $check = $date->format('Y-m-d');
+                                    if(strpos($row, $check) !== false){
+                                        $txvol[$x] += 1;
+                                    }
+                                    $date->sub(new DateInterval('P1D'));
+                                }
+
+                            }
+                            $array = array_reverse($txvol);
+                            echo "data: ['".$array[0]."', '".$array[1]."','".$array[2]."','".$array[3]."','".$array[4]."','".$array[5]."','".$array[6]."',],";
+                        ?>
+                        backgroundColor: 'rgba(0, 255, 0, 0.6)',
+                        borderColor: 'rgba(0, 0, 0, 0.3)',
+                        borderWidth: 2
+                    }]
+                },
+            });
+            </script>
             </div>
         </div>
         <div class="row">

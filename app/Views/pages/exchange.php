16274ad8397179956838bd62a181885f814c7d18
877chs7oe99hk9wf <admin@linuxmasterrace.com>
2020-03-24 13:16:30 +0200

    add placeholder exchange

--- app/Views/pages/exchange.php
+++ app/Views/pages/exchange.php
@@ -1,8 +1,33 @@
 <div class="container">
     <br>
-    <?= \Config\Services::validation()->listErrors(); ?>
     <div class="container mt-5">
-        <h1 class="text-center"> Exchange</h1>
+    <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+            
+            <a class="nav-link" href="#">
+                <a href="/stonkinfo/#">STO</a> 
+            </a>
+            <a class="nav-link" href="#">
+                <a href="/stonkinfo/#">STO</a> 
+            </a>
+            <a class="nav-link" href="#">
+                <a href="/stonkinfo/#">STO</a> 
+            </a>
+            </li>
+        </ul>
+    </nav>
+    <div class="col-md-4">
+    <p>StonkName - <a href="/issuerinfo/#">Issuer</a> - CurPrice - Change1W - Change1D</p>
+    <p>StonkDescription</p>
+    <a href="/quicktrade"><button class="btn btn-success">Buy/Sell</button></a>
+    <img src="https://cdn.archi.fi/pricegraph.png" width="150%"></img>
+    </div>
+    </div>
+
+
     </div>
   
 </div>

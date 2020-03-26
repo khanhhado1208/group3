ce11c2a02068b29bb420626c05ed4f52cfac839d
Henrik Kurkela <henrik.kurkela@gmail.com>
2020-03-26 18:43:39 +0200

    Better Transaction Text for Signup Bonus

--- app/Controllers/Account.php
+++ app/Controllers/Account.php
@@ -238,7 +238,7 @@ class Account extends BaseController
                     'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
                 ]);
                 $this->setErrorState('success', 'New user created, please login');
-                $model->money_transaction($this->request->getVar('username'), 10, "Deposit");
+                $model->money_transaction($this->request->getVar('username'), 10, "Sign-Up Bonus");
                 $pageController->get('login');
             }
         }

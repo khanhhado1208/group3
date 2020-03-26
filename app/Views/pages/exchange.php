666ca3f0ecce4e1bd38fb068a55a15144cb2cc5c
877chs7oe99hk9wf <admin@linuxmasterrace.com>
2020-03-26 18:04:17 +0200

    Add tab layout in exchange

--- app/Views/pages/exchange.php
+++ app/Views/pages/exchange.php
@@ -1,33 +1,29 @@
 <div class="container">
-    <br>
-    <div class="container mt-5">
-    <div class="row">
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-        <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-            
-            <a class="nav-link" href="#">
-                <a href="/stonkinfo/#">STO</a> 
-            </a>
-            <a class="nav-link" href="#">
-                <a href="/stonkinfo/#">STO</a> 
-            </a>
-            <a class="nav-link" href="#">
-                <a href="/stonkinfo/#">STO</a> 
-            </a>
-            </li>
-        </ul>
-    </nav>
-    <div class="col-md-4">
-    <p>StonkName - <a href="/issuerinfo/#">Issuer</a> - CurPrice - Change1W - Change1D</p>
-    <p>StonkDescription</p>
+<br>
+<div class='row'>
+
+<?php 
+echo '<div class="nav flex-column nav-pills col-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">';
+echo '<a class="nav-link active" id="v-pills-'.$stonkids[0].'-tab" data-toggle="pill" href="#v-pills-'.$stonkids[0].'" role="tab" aria-controls="v-pills-'.$stonkids[0].'" aria-selected="true">';
+echo $stonknames[2].'</a>';
+for($x = 1; $x <= sizeof($stonkids) - 1; $x++){
+    echo '<a class="nav-link" id="v-pills-'.$stonkids[$x].'-tab" data-toggle="pill" href="#v-pills-'.$stonkids[$x].'" role="tab" aria-controls="v-pills-'.$stonkids[$x].'" aria-selected="false">';
+    echo $stonknames[$x+2].'</a>';
+}
+echo '<div class="tab-content col-9" id="v-pills-tabContent">';
+echo '<div class="tab-pane fade show active" id="v-pills-'.$stonkids[0].'" role="tabpanel" aria-labelledby="v-pills-'.$stonkids[0].'-tab">
+<p class=h3>'.$stonknames[2].'</p>
+<p class=h6><a href="#">Issuername</a></p>
+<a href="/quicktrade"><button class="btn btn-success">Buy/Sell</button></a>
+</div>';
+for($x = 1; $x <= sizeof($stonkids) - 1; $x++){
+    echo '<div class="tab-pane fade" id="v-pills-'.$stonkids[$x].'" role="tabpanel" aria-labelledby="v-pills-'.$stonkids[$x].'-tab">
+    <p class=h3>'.$stonknames[$x+2].'</p>
+    <p class=h6><a href="#">Issuername</a></p>
     <a href="/quicktrade"><button class="btn btn-success">Buy/Sell</button></a>
-    <img src="https://cdn.archi.fi/pricegraph.png" width="150%"></img>
-    </div>
-    </div>
+    </div>';
+}
+echo '</div></div>';
+?>
 
-
-    </div>
-  
-</div>
+</div></div>

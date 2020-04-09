97028527278c08de0bc8cc8612817a0371f4afc5
Henrik Kurkela <henrik.kurkela@gmail.com>
2020-04-09 19:53:20 +0300

    Change Choose Avatar Layout

--- app/Views/pages/avatar.php
+++ app/Views/pages/avatar.php
@@ -1,47 +1,33 @@
 <style>
-.carousel-control-next-icon,
-.carousel-control-prev-icon {
-    filter: invert(1);
+.avatar {
+    width: 200px;
+    height: 200px;
+    margin: 10px;
+    float: left;
+    object-fit: cover;
+}
+.avatar:hover {
+    cursor: pointer;
+}
+.clearboth {
+    clear: both;
 }
 </style>
 
 <div class="container">
-
-<div class="container">
-
-	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false">
-		<div class="carousel-inner">
-
-			<?php
-                $files = glob('img/*.{jpg,png,gif}', GLOB_BRACE);
-                $active = false;
-                foreach ($files as $file) {
-                    if (!$active) {
-                        echo '<div class="carousel-item active">';
-                        $active = true;
-                    } else {
-                        echo '<div class="carousel-item">';
-                    }
-                    echo '<img class="d-block w-100" src="'.$file.'" onclick=selectAvatar("'.$file.'")>';
-                    echo '</div>';
-                }
-          ?>
-		</div>
-		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
-</div>
+<p class=h3>Click to choose your avatar</p>
+<?php
+    $files = glob('img/*.{jpg,png,gif}', GLOB_BRACE);
+    foreach ($files as $file) {
+        echo '<img class="d-block avatar" src="'.$file.'" onclick=selectAvatar("'.$file.'")>';
+    }
+?>
+<div class="clearboth"></div>
 
 <form action="<?php echo base_url('/account/avatar') ?>" method="post" accept-charset="utf-8" id="form">
     <input type="hidden" name="file" id="file">
 </form>
-
+</div>
 <script>
 
 function selectAvatar(avatar) {
